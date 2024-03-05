4b2cc2e6ca836766a701f893b4867e76c9704e21
aetaja <158024557+aetaja@users.noreply.github.com>
2024-03-05 08:02:32 +0700

    baru
    baru

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
-                    </div> 
+                    </div>
                 </div><!-- /.container-fluid -->
             </div>
         @endif
